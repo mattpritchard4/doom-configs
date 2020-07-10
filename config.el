82a73c9b4117bd7b9f232a2d74b4efe92ff8d56e
Matt Pritchard <mattpritchard@Admins-MacBook-Pro.local>
2020-07-10 12:43:00 -0400

    remove which-key

--- config.el
+++ config.el
@@ -51,8 +51,8 @@
 
 ;; Font
 
-(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 24)
-      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 24))
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 14)
+      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 14))
 
 ;; Disable irritating ts-mode smartparens behavior
 (add-hook! 'typescript-mode-hook (local-unset-key "\'"))
@@ -66,21 +66,59 @@
 ;; angular lsp
 
 ;; linux
-(setq lsp-clients-angular-language-server-command
-  '("node"
-    "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules/@angular/language-server"
-    "--ngProbeLocations"
-    "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
-    "--tsProbeLocations"
-    "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
-    "--stdio"))
-
-;; mac
 ;; (setq lsp-clients-angular-language-server-command
 ;;   '("node"
-;;     "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules/@angular/language-server"
+;;     "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules/@angular/language-server"
 ;;     "--ngProbeLocations"
-;;     "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
+;;     "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
 ;;     "--tsProbeLocations"
-;;     "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
+;;     "/home/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
 ;;     "--stdio"))
+
+;; mac
+(setq lsp-clients-angular-language-server-command
+  '("node"
+    "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules/@angular/language-server"
+    "--ngProbeLocations"
+    "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
+    "--tsProbeLocations"
+    "/Users/mattpritchard/.nvm/versions/node/v10.16.2/lib/node_modules"
+    "--stdio"))
+
+;; projectile
+
+(add-to-list 'projectile-globally-ignored-directories "*/node_modules/")
+
+(defun shell-cmd (cmd)
+  "Returns the stdout output of a shell command or nil if the command returned
+   an error"
+  (car (ignore-errors (apply 'process-lines (split-string cmd)))))
+
+(defun reason-cmd-where (cmd)
+  (let ((where (shell-cmd cmd)))
+    (if (not (string-equal "unknown flag ----where" where))
+      where)))
+
+(let* ((refmt-bin (or (reason-cmd-where "refmt ----where")
+                      (shell-cmd "which refmt")
+                      (shell-cmd "which bsrefmt")))
+       (merlin-bin (or (reason-cmd-where "ocamlmerlin ----where")
+                       (shell-cmd "which ocamlmerlin")))
+       (merlin-base-dir (when merlin-bin
+                          (replace-regexp-in-string "bin/ocamlmerlin$" "" merlin-bin))))
+  ;; Add merlin.el to the emacs load path and tell emacs where to find ocamlmerlin
+  (when merlin-bin
+    (add-to-list 'load-path (concat merlin-base-dir "share/emacs/site-lisp/"))
+    (setq merlin-command merlin-bin))
+
+  (when refmt-bin
+    (setq refmt-command refmt-bin)))
+
+(require 'reason-mode)
+(require 'merlin)
+(add-hook 'reason-mode-hook (lambda ()
+                              (add-hook 'before-save-hook 'refmt-before-save)
+                              (merlin-mode)))
+
+(setq merlin-ac-setup t)
+(after! which-key  (which-key-mode -1))
